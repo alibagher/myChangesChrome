de0d3ce2042c12b46f99e2bc076a6143707fc76e
Ali Bagheri <a6bagher@uwaterloo.ca>
2024-07-07 18:52:03 -0400

    Update utilities_api.cc

--- utilities/chrome/browser/extensions/api/utilities/utilities_api.cc
+++ utilities/chrome/browser/extensions/api/utilities/utilities_api.cc
@@ -19,6 +19,12 @@
 #include "content/public/browser/clipboard_monitor.h"
 #include "extensions/browser/event_router.h"
 
+#include "chrome/browser/extensions/api/utilities/utilities_api.h"
+#include "chrome/browser/profiles/profile.h"
+#include "extensions/browser/event_router.h"
+#include "extensions/browser/extensions_browser_client.h"
+#include "chrome/browser/extensions/api/constants.h"
+
 namespace extensions {
 
 std::string GetVersion() {
@@ -76,64 +82,29 @@ bool IsFirstRun() {
 }
 
 /////// Initializations for onClipboardContentChanged ////////////
-https://source.chromium.org/chromium/chromium/src/+/main:extensions/browser/api/clipboard/clipboard_api.cc;l=15;bpv=1;bpt=0
-https://source.chromium.org/chromium/chromium/src/+/main:ui/base/clipboard/clipboard_monitor.h;drc=fe91e486972e3ce15459cea8765892913e487824;bpv=1;bpt=0;l=1
-void TabsEventRouter::DispatchTabUpdatedEvent(
-    WebContents* contents,
-    const std::set<std::string> changed_property_names) {
-  DCHECK(!changed_property_names.empty());
-  DCHECK(contents);
+// https://source.chromium.org/chromium/chromium/src/+/main:extensions/browser/api/clipboard/clipboard_api.cc;l=15;bpv=1;bpt=0
+// https://source.chromium.org/chromium/chromium/src/+/main:ui/base/clipboard/clipboard_monitor.h;drc=fe91e486972e3ce15459cea8765892913e487824;bpv=1;bpt=0;l=1
+const char ClipboardContentChangedEvent::kEventName[] = "onClipboardContentChanged";
+const char ClipboardContentChangedEvent::event_name_[] = "chrome.utilities.onClipboardContentChanged";
 
-  Profile* profile = Profile::FromBrowserContext(contents->GetBrowserContext());
-
-  auto event = std::make_unique<Event>(
-      events::TABS_ON_UPDATED, api::tabs::OnUpdated::kEventName,
-      // The event arguments depend on the extension's permission. They are set
-      // in WillDispatchTabUpdatedEvent().
-      base::Value::List(), profile);
-  event->user_gesture = EventRouter::USER_GESTURE_NOT_ENABLED;
-  event->will_dispatch_callback =
-      base::BindRepeating(&WillDispatchTabUpdatedEvent, contents,
-                          std::move(changed_property_names));
-  EventRouter::Get(profile)->BroadcastEvent(std::move(event));
-}
-
-
-
-#include "chrome/browser/extensions/api/utilities/utilities_api.h"
-#include "chrome/browser/profiles/profile.h"
-#include "extensions/browser/event_router.h"
-#include "extensions/browser/extensions_browser_client.h"
-#include "chrome/browser/extensions/api/constants.h"
-
-UtilitiesAPI::UtilitiesAPI() {
+UtilitiesClipboardAPI::UtilitiesClipboardAPI() {
   ui::ClipboardMonitor::GetInstance()->AddObserver(this);
 }
 
-UtilitiesAPI::~UtilitiesAPI() {
+UtilitiesClipboardAPI::~UtilitiesClipboardAPI() {
   ui::ClipboardMonitor::GetInstance()->RemoveObserver(this);
 }
 
-void UtilitiesAPI::OnClipboardDataChanged() {
-  // Call your custom method when the clipboard data changes
-  OnClipboardContentChanged();
-}
-
-void UtilitiesAPI::OnClipboardContentChanged() {
-  // Logic to fire the event to the JavaScript side
-  Profile* profile = Profile::FromBrowserContext(
-      ExtensionsBrowserClient::Get()->GetMainContext());
-  EventRouter* event_router = EventRouter::Get(profile);
-  if (event_router) {
+void UtilitiesClipboardAPI::OnClipboardDataChanged() {
+      Profile* profile = Profile::FromBrowserContext(web_contents()->GetBrowserContext());
+    
     auto event = std::make_unique<Event>(
-        events::kOnClipboardContentChanged, 
-        "utilities.onClipboardContentChanged", 
-        std::make_unique<base::ListValue>());
-    event_router->BroadcastEvent(std::move(event));
-  }
+      ClipboardContentChangedEvent::kEventName,
+      ClipboardContentChangedEvent::event_name_,
+      base::Value::List(), profile);
+    event->user_gesture = EventRouter::USER_GESTURE_NOT_ENABLED;
+    
+    EventRouter::Get(profile)->BroadcastEvent(std::move(event));
 }
-
-
-
 
 } // namespace extensions

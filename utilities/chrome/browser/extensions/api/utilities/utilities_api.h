900586db4a7091bbdbfa66faabea126c44729237
Ali Bagheri <a6bagher@uwaterloo.ca>
2024-07-07 16:17:51 -0400

    Update utilities_api.h

--- utilities/chrome/browser/extensions/api/utilities/utilities_api.h
+++ utilities/chrome/browser/extensions/api/utilities/utilities_api.h
@@ -4,26 +4,27 @@
 #include <string>
 
 #include "base/macros.h"
-#include "extensions/browser/browser_context_keyed_api_factory.h"
+#include "extensions/browser/extension_function.h"
+#include "ui/base/clipboard/clipboard_monitor.h"
+#include "ui/base/clipboard/clipboard_observer.h"
 #include "extensions/browser/event_router.h"
 
-namespace extensions {
+namespace chrome {
 namespace utilities {
-// // Permission for accessing utilities namespace
-// const char* const kUtilitiesPermission = "utilities";
 
-std::string GetVersion();
+class UtilitiesAPI: public ui::ClipboardObserver{
+ public:
+  UtilitiesAPI();
+  ~UtilitiesAPI() override;
 
-bool IsFirstRun();
+  std::string GetVersion();
 
-// // Event fired when clipboard content changes
-// typedef base::Callback<void(const std::string&)> ClipboardContentChangedCallback;
+  bool IsFirstRun();
 
-// onClipboardContentChanged
-// https://source.chromium.org/chromium/chromium/src/+/main:ui/base/clipboard/clipboard_monitor.h;drc=fe91e486972e3ce15459cea8765892913e487824;bpv=1;bpt=0;l=1
-static void OnClipboardContentChanged();
-
+  void OnClipboardDataChanged() override;
+  void OnClipboardContentChanged();
+};
 
 }  // namespace utilities
-}  // namespace extensions
+}  // namespace chrome
 #endif  // UTILITIES_H_

c1ce841a81ef1bbb2e89faa5aa7d2163b70349cb
Ali Bagheri <a6bagher@uwaterloo.ca>
2024-07-06 22:08:16 -0400

    Update utilities_api.h

--- utilities/chrome/browser/extensions/api/utilities/utilities_api.h
+++ utilities/chrome/browser/extensions/api/utilities/utilities_api.h
@@ -5,6 +5,11 @@
 
 #include <string>
 
+#include "base/macros.h"
+#include "extensions/browser/browser_context_keyed_api_factory.h"
+#include "extensions/browser/event_router.h"
+
+namespace extensions {
 namespace utilities {
 // // Permission for accessing utilities namespace
 // const char* const kUtilitiesPermission = "utilities";
@@ -13,9 +18,13 @@ std::string GetVersion();
 
 bool IsFirstRun();
 
-// Event fired when clipboard content changes
+// // Event fired when clipboard content changes
 typedef base::Callback<void(const std::string&)> ClipboardContentChangedCallback;
-void onClipboardContentChanged(const ClipboardContentChangedCallback& callback);
+
+// onClipboardContentChanged
+
+https://source.chromium.org/chromium/chromium/src/+/main:ui/base/clipboard/clipboard_monitor.h;drc=fe91e486972e3ce15459cea8765892913e487824;bpv=1;bpt=0;l=1
 
 }  // namespace utilities
+}  // namespace extensions
 #endif  // UTILITIES_H_
